4cc3b8c3f7dbaf8fd9caaf71772415d00b135f8b
tobiasbkreamer <tobias@kraemerne.dk>
2023-08-31 15:08:23 +0200

    Workflow coms

---
+++ src/main/java/com/example/githubaction22/GitHubAction22Application.java
@@ -0,0 +1,13 @@
+package com.example.githubaction22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubAction22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubAction22Application.class, args);
+    }
+
+}
